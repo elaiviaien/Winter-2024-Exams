b6aca7fb40b2cb9801d3c00eae8cb854e153874c
Kv1lon <hiphopd4@gmail.com>
2024-01-14 11:05:13 +0000

    compare | logic simplified

--- Tasks/compare.py
+++ Tasks/compare.py
@@ -5,14 +5,17 @@ def compare(first_values, *parameters_list):
     second_values = parameters_list[0]
     first_keys = list(first_values.keys())
     second_keys = list(second_values.keys())
-    if '-'.join(first_keys) != '-'.join(second_keys): return False
+    if '-'.join(first_keys) != '-'.join(second_keys):
+        return False
+
     all_equal = True
     for key in first_keys:
-        if first_values[key] == second_values[key]: all_equal = all_equal and True
-        else:
-            all_equal = all_equal and False
+        if first_values[key] != second_values[key]:
+            all_equal = False
+            break
 
     return all_equal
+
 
 def run(*args):
     return compare(*args)
