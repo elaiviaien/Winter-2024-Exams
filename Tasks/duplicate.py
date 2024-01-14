023efb6b2fcfc19a7fcfd9ff3dd2972712e69465
Kv1lon <hiphopd4@gmail.com>
2024-01-14 12:43:10 +0000

    duplicates | simplified

--- Tasks/duplicate.py
+++ Tasks/duplicate.py
@@ -2,13 +2,11 @@
 
 
 def duplicate(value, number):
-    if number <= 0:return []
-    else:
-        duplicates=[]
-        for i in range(number):
-            duplicates.append(None)
-            duplicates[i] = value
-        return duplicates
+    duplicates = []
+    for i in range(number):
+        duplicates.append(value)
+    return duplicates
+
 
 def run(*args):
     return duplicate(*args)
