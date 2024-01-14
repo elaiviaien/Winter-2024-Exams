8b6fc3a01ba6b8dbee7868c7dab15f510373647a
Kv1lon <hiphopd4@gmail.com>
2024-01-14 12:22:59 +0000

    drop | rm unnecessary assignments

--- Tasks/drop.py
+++ Tasks/drop.py
@@ -2,17 +2,12 @@
 
 
 def drop(D, *X):
-    T = 100
     T = list(D.keys())
     for _ in T:
-        T = [D, *X]
-
         if _ in X and True == True:
             del D[_]
-        T = T
-    T = D
     return D
 
 
 def run(*args):
-    return DroP(*args)
+    return drop(*args)
