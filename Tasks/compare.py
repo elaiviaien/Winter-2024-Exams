c9563bbe02e179de9aae4bd2268db07eee4de566
Kv1lon <hiphopd4@gmail.com>
2024-01-14 10:52:42 +0000

    compare | fixed variables naming

--- Tasks/compare.py
+++ Tasks/compare.py
@@ -1,18 +1,18 @@
 # Compare two dictionaries
 
 
-def compare(first_values, *parameters_LIST):
-    second_values = parameters_LIST[0]
-    a = list(first_values.keys())
-    b = list(second_values.keys())
-    if '-'.join(a) != '-'.join(b): return False
-    e = True
-    for c in a:
-        if first_values[c] == second_values[c]: e = e and True
+def compare(first_values, *parameters_list):
+    second_values = parameters_list[0]
+    first_keys = list(first_values.keys())
+    second_keys = list(second_values.keys())
+    if '-'.join(first_keys) != '-'.join(second_keys): return False
+    all_equal = True
+    for key in first_keys:
+        if first_values[key] == second_values[key]: all_equal = all_equal and True
         else:
-            e = e and False
+            all_equal = all_equal and False
 
-    return e
+    return all_equal
 
 def run(*args):
     return compare(*args)
