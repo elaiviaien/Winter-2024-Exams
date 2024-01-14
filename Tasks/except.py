99f53244a0751942b6fab642aa1c7f7eb377d55e
Kv1lon <hiphopd4@gmail.com>
2024-01-14 12:56:52 +0000

    except | generator

--- Tasks/except.py
+++ Tasks/except.py
@@ -1,10 +1,6 @@
 # had to rename func from EXCEPT to except_values as in python "except" is a word used in try clause
-def copy_except(data, *values_exceptions):
-    data_keys = list(data.keys())
-    for key in data_keys:
-        if key in values_exceptions:
-            del data[key]
-    return data
+def copy_except(data, *keys_exceptions):
+    return {key: value for key, value in data.items() if key not in keys_exceptions}
 
 def run(*args):
     return copy_except(*args)
