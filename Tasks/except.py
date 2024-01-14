201c276bf54791b6ea6e30c76269ceaae319693c
Kv1lon <hiphopd4@gmail.com>
2024-01-14 12:57:21 +0000

    except | type hinting

--- Tasks/except.py
+++ Tasks/except.py
@@ -1,6 +1,7 @@
 # had to rename func from EXCEPT to except_values as in python "except" is a word used in try clause
-def copy_except(data, *keys_exceptions):
+def copy_except(data: dict, *keys_exceptions: list) -> dict:
     return {key: value for key, value in data.items() if key not in keys_exceptions}
+
 
 def run(*args):
     return copy_except(*args)
