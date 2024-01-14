467f84864c23198991afb511d97145c5b54be0bd
Kv1lon <hiphopd4@gmail.com>
2024-01-14 12:19:59 +0000

    day | fixed variables naming

--- Tasks/day.py
+++ Tasks/day.py
@@ -1,11 +1,11 @@
 # Get day number
 
 
-def parse_day(s):
-    D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-    for i, word in enumerate(D, start=1):
-        if s.startswith(word.lower()):
-            return i
+def parse_day(input_day):
+    abbreviated_days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+    for index, abbreviated_day in enumerate(abbreviated_days, start=1):
+        if input_day.startswith(abbreviated_day.lower()):
+            return index
     return -1
 
 
