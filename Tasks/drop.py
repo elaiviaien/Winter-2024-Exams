6cdaa77ef9749430877fbe9d4cf4316210c0cfbc
Kv1lon <hiphopd4@gmail.com>
2024-01-14 12:36:00 +0000

    drop | generator

--- Tasks/drop.py
+++ Tasks/drop.py
@@ -2,11 +2,7 @@
 
 
 def drop(data, *keys_to_drop):
-    keys = list(data.keys())
-    for key in keys:
-        if key in keys_to_drop:
-            del data[key]
-    return data
+    return {key: value for key, value in data.items() if key not in keys_to_drop}
 
 
 def run(*args):
