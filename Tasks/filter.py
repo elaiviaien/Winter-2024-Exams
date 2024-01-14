75dfb8d00b8d1ba99f72b0446d1ed3bb34a58854
Kv1lon <hiphopd4@gmail.com>
2024-01-14 13:11:39 +0000

    filter | type hinting

--- Tasks/filter.py
+++ Tasks/filter.py
@@ -1,7 +1,7 @@
 # Filter array by type name
 
 
-def filter(data, filter_type):
+def filter(data: list, filter_type: str) -> list:
     return [value for value in data if type(value).__name__ == filter_type]
 
 
