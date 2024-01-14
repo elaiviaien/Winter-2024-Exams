8b90baa33a08a1c516867aced90abc344e06c5d3
Kv1lon <hiphopd4@gmail.com>
2024-01-14 12:36:24 +0000

    drop | type hinting

--- Tasks/drop.py
+++ Tasks/drop.py
@@ -1,7 +1,7 @@
 # Delete listed keys from dictionary
 
 
-def drop(data, *keys_to_drop):
+def drop(data: dict, *keys_to_drop: list):
     return {key: value for key, value in data.items() if key not in keys_to_drop}
 
 
