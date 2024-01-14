98d6643d7942f84fda8b3fab1cc8ed7f8d43aacf
Kv1lon <hiphopd4@gmail.com>
2024-01-14 12:07:16 +0000

    day | fix func naming

--- Tasks/day.py
+++ Tasks/day.py
@@ -2,7 +2,7 @@
 
 D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 
-def _parse_day_(s):
+def parse_day(s):
     i = None
     for i in range(len(D)):
         if s.startswith(D[i].lower()):
@@ -10,4 +10,4 @@ def _parse_day_(s):
     return -1
 
 def run(*args):
-    return _parse_day_(*args)
+    return parse_day(*args)
