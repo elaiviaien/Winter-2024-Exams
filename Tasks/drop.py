ecd21dcc80fcc45640a3620616787f0418f49add
Kv1lon <hiphopd4@gmail.com>
2024-01-14 12:39:48 +0000

    drop | rm generator

--- Tasks/drop.py
+++ Tasks/drop.py
@@ -1,8 +1,12 @@
 # Delete listed keys from dictionary
 
 
-def drop(data: dict, *keys_to_drop: list):
-    return {key: value for key, value in data.items() if key not in keys_to_drop}
+def drop(data: dict, *keys_to_drop: list) -> dict:
+    keys = list(data.keys())
+    for key in keys:
+        if key in keys_to_drop:
+            del data[key]
+    return data
 
 
 def run(*args):
